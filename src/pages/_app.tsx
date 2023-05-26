e65ba815ae8b509473944f1ae04e8dffbeb806ff
Guilherme <guilhermemagno09@gmail.com>
2023-05-26 18:41:06 -0300

    config app and tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "./components/navbar";
+
+import { Nunito } from "next/font/google";
+
+const font = Nunito({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <div className="pb-20 pt-28">
+        <Component className={font.className} {...pageProps} />
+      </div>
+    </>
+  );
 }
