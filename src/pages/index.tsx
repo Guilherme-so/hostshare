9088d13c7ca47a0f4f6c873af5dfcaebe2d89ee8
Guilherme <guilhermemagno09@gmail.com>
2023-05-27 21:55:57 -0300

    global styling for gallery modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,8 +34,7 @@ const Home = ({ data }: HomeProps) => {
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const url =
-    "https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685215979593&signature=pABV_JxGSsBZg2ie91bnEcQgxj0_WI7wSe8ScfO20Pk&downloadName=listings.json";
-
+    "https://file.notion.so/f/s/24643894-e5c3-4c40-974a-52594f581e03/listings.json?id=f795dab6-14d4-48a9-9567-c72151d311a2&table=block&spaceId=f2ea7328-64a4-4f18-bacc-df6c9ac3d888&expirationTimestamp=1685305926635&signature=ugEcSyhZUFrHgWuVAsrg4jpxn84O5fdx58sCoqsV7WI&downloadName=listings.json";
   const resp = await fetch(url);
   const data = await resp.json();
 
