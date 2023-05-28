c6f2e916bb09e3652e04b4608d6c36e42f1a82d9
Guilherme <guilhermemagno09@gmail.com>
2023-05-28 19:46:51 -0300

    adding error and loading page

---
+++ src/pages/error.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+export default function Error({ error }: { error: Error }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return <EmptyState 
+  title="uh oh"
+  subtitle="Something went wrong!"
+  />;
+}
